7d8a04bc3a5a3235bd652db41803153f48bc3a2e
NicsonMightman <lolhelost@gmail.com>
2018-07-04 14:01:05 +0600

    Added an exception for division

--- CalculateMinLOb/CalculateMinLOb.Tests/TwoArgumentsFolder/DivisionTestStrong.cs
+++ CalculateMinLOb/CalculateMinLOb.Tests/TwoArgumentsFolder/DivisionTestStrong.cs
@@ -1,4 +1,5 @@
-﻿using CalculateMinLOb.TwoArgumentsFolder;
+﻿using System;
+using CalculateMinLOb.TwoArgumentsFolder;
 using NUnit.Framework;
 
 namespace CalculateMinLOb.Tests.TwoArgumentsFolder
@@ -14,10 +15,15 @@ namespace CalculateMinLOb.Tests.TwoArgumentsFolder
         public void CalculateDivisionTestStrong(double firstValue, double secondValue, double expected)
         {
 
-
-            ITwoArgumentsCalculator calculator = new Division();
+            ITwoArgumentsCalculator calculator = TwoArgumentsFactory.CreateCalculator("div");
             double result = calculator.Calculate(26, 13);
             Assert.AreEqual(2, result);
+        }
+        [Test]
+        public void DivisionExeptionTest()
+        {
+            ITwoArgumentsCalculator calculator = TwoArgumentsFactory.CreateCalculator("div");
+            Assert.Throws<Exception>(() => calculator.Calculate(5, 0));
         }
     }
 }
--- CalculateMinLOb/CalculateMinLOb/TwoArgumentsFolder/Division.cs
+++ CalculateMinLOb/CalculateMinLOb/TwoArgumentsFolder/Division.cs
@@ -1,9 +1,16 @@
-﻿namespace CalculateMinLOb.TwoArgumentsFolder
+﻿using System;
+
+namespace CalculateMinLOb.TwoArgumentsFolder
 {
     public class Division : ITwoArgumentsCalculator
     {
         public double Calculate(double firstArgument, double secondArgument)
         {
+            if (secondArgument == 0)
+            {
+                throw new Exception("div");
+            }
+
             return firstArgument / secondArgument;
         }
     }
