3d11989ed524bb56fe8455c385aa41984ec871a9
NicsonMightman <lolhelost@gmail.com>
2018-07-06 11:29:41 +0600

    Added web interface

--- CalculateMinLOb/CalculateMinLOb/OneArgumentFolder/ArcCosinus.cs
+++ CalculateMinLOb/CalculateMinLOb/OneArgumentFolder/ArcCosinus.cs
@@ -4,7 +4,11 @@ namespace CalculateMinLOb.OneArgumentFolder
 {
     public class ArcCosinus : IOneArgumentCalculator
     {
-
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="firstArgument"></param>
+        /// <returns></returns>
         public double Calculate(double firstArgument)
         {
             if ((-1.0 <= firstArgument) && (firstArgument <= 1.0))
---
+++ CalculateMinLOb/WebInterface/Controllers/HomeController.cs
@@ -0,0 +1,75 @@
+﻿using System.Web.Mvc;
+using CalculateMinLOb.TwoArgumentsFolder;
+using SelectListItem = System.Web.WebPages.Html.SelectListItem;
+
+namespace WebApplication.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.Operation = new SelectListItem[]
+            {
+                new SelectListItem()
+                {
+                    Value = "mult", Text ="Multiply"
+                },
+                new SelectListItem()
+                {
+                    Value = "add", Text ="Addition"
+                },
+                new SelectListItem()
+                {
+                    Value = "minus", Text ="Minus"
+                },
+                new SelectListItem()
+                {
+                    Value = "div", Text ="Division"
+                }
+            };
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(double firstNumber, double secondNumber, string operation)
+        {
+            ITwoArgumentsCalculator calculator = TwoArgumentsFactory.CreateCalculator(operation);
+            double result = calculator.Calculate(firstNumber, secondNumber);
+            ViewBag.Result = result;
+            ViewBag.Operation = new SelectListItem[]
+            {
+                new SelectListItem()
+                {
+                    Value = "mult", Text ="Multiply"
+                },
+                new SelectListItem()
+                {
+                    Value = "add", Text ="Addition"
+                },
+                new SelectListItem()
+                {
+                    Value = "minus", Text ="Minus"
+                },
+                new SelectListItem()
+                {
+                    Value = "div", Text ="Division"
+                }
+            };
+            return View();
+        }
+    }
+}
